b9ce6fec4bec0626dd267b79f85059a7e46c0af4
Asylturatbek <asylturatbek@gmail.com>
2019-11-02 19:49:46 +0600

    added templates and created html and css files

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'AsylDoor',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'November 2, 2019'
+    },
+    {
+        'author': 'Esen Amanovich',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'November 3, 2019'
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
-
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About Motherfuckkkeeeer</h1>')
-
-
-# Create your views here.
+    return render(request, 'blog/about.html',{'title': 'About'})
